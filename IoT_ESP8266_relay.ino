8927bc1044bf6a0ada9b7600f07139b17386694d
dacb <dacb@comcast.net>
2015-05-31 02:25:25 -0700

    building; more advanced support and function collapsing

--- IoT_ESP8266_relay.ino
+++ IoT_ESP8266_relay.ino
@@ -1,3 +1,12 @@
+/*
+ESP8266 only Internet-of-Things device
+
+Currently supports ESP-01 w/ a relay or DS18B20 on GPIO0 and GPIO2.
+The relay support is actually just driving the GPIOs with logic
+	inverted.  That is, in relay mode, setting the relay to true (on)
+	brings the pin low with a digialwrite.
+*/
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
@@ -13,22 +22,54 @@
 #define IGNORE_GPIO0
 #undef IGNORE_GPIO0
 
+// define feature set
+#define GPIO0_RELAY
+//#define GPIO0_DS18B60
+#define GPIO2_RELAY
+//#define GPIO2_DS18B60
+
+#ifdef defined(GPIO0_RELAY) && defined(GPIO0_DS18B60)
+	#error "GPIO0 cannot be assigned to a relay AND a DS18B60 simultaneously"
+#elif defined(GPIO2_RELAY) && defined(GPIO2_DS18B60)
+	#error "GPIO2 cannot be assigned to a relay AND a DS18B60 simultaneously"
+#endif
+
+#define HOSTNAME_MAX_LENGTH 12
+
+// state
+typedef struct stateStruct {
+	byte mac[6];
+	char hostname[HOSTNAME_MAX_LENGTH];
+	uint32_t chipId;
+	uint32_t flashChipId;
+	uint32_t flashChipSize;
+	uint32_t flashChipSpeed;
+#ifdef GPIO0_RELAY
+	bool gpio0_relay;
+#elif GPIO0_DS18B60
+	float gpio0_temperature;
+#endif
+#ifdef GPIO2_RELAY
+	bool gpio2_relay;
+#elif GPIO2_DS18B60
+	float gpio2_temperature;
+#endif
+	IPAddress ip;
+} state_t;
+state_t state;
+
 // prototypes
 void handleRoot();
+#ifdef GPIO0_RELAY
 void handleRelay0On(void);
 void handleRelay0Off(void);
-void handleRelay1On(void);
-void handleRelay1Off(void);
+#endif
+#ifdef GPIO2_RELAY
+void handleRelay2On(void);
+void handleRelay2Off(void);
+#endif
 void handleNotFound(void);
 void sendIndexPage(void);
-
-// MAC address of this unit
-byte mac[6];
-// mDNS name
-const int MDNS_NAME_MAX_LENGTH = 12;
-char mDNSName[MDNS_NAME_MAX_LENGTH];
-// state of the relays
-bool relay0 = false, relay1 = false;
 
 // multicast DNS responder
 MDNSResponder mdns;
@@ -36,29 +77,29 @@ MDNSResponder mdns;
 // Webserver on port 80
 ESP8266WebServer server(80);
 
-void relay0On(void) {
-    relay0 = true;
+#ifdef GPIO0_RELAY
+void gpio0_relay(bool on) {
+    state.gpio0_relay = on;
 #ifndef IGNORE_GPIO0
-    digitalWrite(0, LOW);
+	if (on) {
+    	digitalWrite(0, LOW);
+	} else {
+    	digitalWrite(0, HIGH);
+	}
 #endif
 }
-
-void relay0Off(void) {
-    relay0 = false;
-#ifndef IGNORE_GPIO0
-    digitalWrite(0, HIGH);
 #endif
-}
 
-void relay1On(void) {
-    relay1 = true;
-    digitalWrite(2, LOW);
+#ifdef GPIO2_RELAY
+void gpio2_relay(bool on) {
+    state.gpio2_relay = on;
+	if (on) {
+    	digitalWrite(2, LOW);
+	} else {
+    	digitalWrite(2, HIGH);
+	}
 }
-
-void relay1Off(void) {
-    relay1 = false;
-    digitalWrite(2, HIGH);
-}
+#endif
 
 // setup the output serial port (used for debugging)
 // connect to the wifi AP
@@ -69,15 +110,27 @@ void setup(void)
 {    
     Serial.begin(115200);
 
-    // initialize pins & relays
+	// setup the basic state structure
+	state.chipId = ESP.getChipId();
+	state.flashChipId = ESP.getFlashChipId();
+	state.flashChipSize = ESP.getFlashChipSize();
+	state.flashChipSpeed = ESP.getFlashChipSpeed();
+
+    // initialize pins features
+#ifdef GPIO0_RELAY
     // relay 0
+    Serial.println("GPIO0 controls a relay");
 #ifndef IGNORE_GPIO0
     pinMode(0, OUTPUT);
 #endif
-    relay0Off();
-    // relay 1
+    gpio0_relay(false);
+#endif
+#ifdef GPIO2_RELAY
+    // relay 2
+    Serial.println("GPIO2 controls a relay");
     pinMode(2, OUTPUT);
-    relay1Off();
+    gpio2_relay(false);
+#endif
 
     // connect to WiFi network
     WiFi.begin(ssid, password);
@@ -92,27 +145,16 @@ void setup(void)
     Serial.print("Connected to ");
     Serial.println(ssid);
     Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
+	state.ip = WiFi.localIP();
+    Serial.println(state.ip);
     // get mac address and assemble hostname for mDNS
-    WiFi.macAddress(mac);
-	Serial.print("MAC: ");
-	Serial.print(mac[5],HEX);
-	Serial.print(":");
-	Serial.print(mac[4],HEX);
-	Serial.print(":");
-	Serial.print(mac[3],HEX);
-	Serial.print(":");
-	Serial.print(mac[2],HEX);
-	Serial.print(":");
-	Serial.print(mac[1],HEX);
-	Serial.print(":");
-	Serial.println(mac[0],HEX);
-    snprintf(mDNSName, MDNS_NAME_MAX_LENGTH,
-            "ESP_%X%X%X", mac[5], mac[4], mac[3]
+    WiFi.macAddress(state.mac);
+    snprintf(state.hostname, HOSTNAME_MAX_LENGTH,
+            "ESP%X%X%X", state.mac[5], state.mac[4], state.mac[3]
         );
     // register hostname w/ mDNS 
     Serial.println("");
-    if (!mdns.begin(mDNSName, WiFi.localIP())) {
+    if (!mdns.begin(state.hostname, state.ip)) {
         Serial.println("Error setting up MDNS responder!");
         while(1) { 
             delay(500);
@@ -120,14 +162,18 @@ void setup(void)
         }
     }
     Serial.print("mDNS responder started with hostname: ");
-    Serial.println(mDNSName);
+    Serial.println(state.hostname);
 
     // setup web server
     server.on("/", handleRoot);
+#ifdef GPIO0_RELAY
     server.on("/relay/0/on", handleRelay0On);
     server.on("/relay/0/off", handleRelay0Off);
-    server.on("/relay/1/on", handleRelay1On);
-    server.on("/relay/1/off", handleRelay1Off);
+#endif
+#ifdef GPIO2_RELAY
+    server.on("/relay/1/on", handleRelay2On);
+    server.on("/relay/1/off", handleRelay2Off);
+#endif
 	server.onNotFound(handleNotFound);
 	server.begin();
     Serial.println("HTTP server started");
@@ -169,37 +215,41 @@ void sendIndexPage(void) {
     </center>\
   </body>\
 </html>",
-		mDNSName,
+		state.hostname,
         hr, min % 60, sec % 60,
-        (relay0 ? "<b>" : ""), (relay0 ? "</b>" : ""), (relay0 ? "" : "<b>"), (relay0 ? "" : "</b>"),
-        (relay1 ? "<b>" : ""), (relay1 ? "</b>" : ""), (relay1 ? "" : "<b>"), (relay1 ? "" : "</b>")
+        (state.gpio0_relay ? "<b>" : ""), (state.gpio0_relay ? "</b>" : ""), (state.gpio0_relay ? "" : "<b>"), (state.gpio0_relay ? "" : "</b>"),
+        (state.gpio2_relay ? "<b>" : ""), (state.gpio2_relay ? "</b>" : ""), (state.gpio2_relay ? "" : "<b>"), (state.gpio2_relay ? "" : "</b>")
 	);
 	server.send(768, "text/html", temp);
 }
 
+#ifdef GPIO0_RELAY
 void handleRelay0On(void) {
-    Serial.println("turning on relay 0");
-    relay0On();
+    Serial.println("turning on relay on GPIO0");
+    gpio0_relay(true);
     sendIndexPage();
 }
 
 void handleRelay0Off(void) {
-    Serial.println("turning off relay 0");
-    relay0Off();
+    Serial.println("turning off relay on GPIO0");
+    gpio0_relay(false);
+    sendIndexPage();
+}
+#endif
+
+#ifdef GPIO2_RELAY
+void handleRelay2On(void) {
+    Serial.println("turning on relay on GPIO2");
+    gpio2_relay(true);
     sendIndexPage();
 }
 
-void handleRelay1On(void) {
-    Serial.println("turning on relay 1");
-    relay1On();
+void handleRelay2Off(void) {
+    Serial.println("turning off relay on GPIO2");
+    gpio2_relay(false);
     sendIndexPage();
 }
-
-void handleRelay1Off(void) {
-    Serial.println("turning off relay 1");
-    relay1Off();
-    sendIndexPage();
-}
+#endif
 
 void handleRoot(void) {
     sendIndexPage();
@@ -221,4 +271,3 @@ void handleNotFound(void) {
 
 	server.send ( 404, "text/plain", message );
 }
-
